5117a3eeae736f19a181f7527ddb7368c03c5b8a
dalgos <dalgos@dalgos.com>
2019-10-29 01:49:59 +0900

    chore: globat type 작성

---
+++ src/@types/global.d.ts
@@ -0,0 +1,27 @@
+interface DateInfo {
+  currentDate: Date;
+}
+
+interface Display {
+  mode: 'monthly' | 'weekly';
+  weekIndex: number;
+}
+
+interface EventForm {
+  open: boolean;
+  startDate: Date;
+  endDate: Date;
+  mode: 'create' | 'edit';
+}
+
+interface Plan {
+  timestamp: Date;
+  seq: string;
+}
+
+interface CalState {
+  dateInfo: DateInfo;
+  display: Display;
+  eventForm: EventForm;
+  plans: Array<Array<Plan>>;
+}
