86c2a8f068a9fe7065b9d23a4e6b50468c5b4025
dalgos <dalgos@dalgos.com>
2019-10-29 01:49:45 +0900

    chore: selector 작성

---
+++ src/selectors/index.ts
@@ -0,0 +1,7 @@
+export const dateInfoSelector = (state: CalState) => state.dateInfo
+
+export const displaySelector = (state: CalState) => state.display
+
+export const eventFormSelector = (state: CalState) => state.eventForm
+
+export const plansSelector = (state: CalState) => state.plans
