99df722d29edff8ac13a8c6e3c01de9d7fa718d8
dalgos <dalgos@dalgos.com>
2019-10-30 01:49:10 +0900

    chore: 일정 목록 조회 기능 추가

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,5 +1,23 @@
-import { } from 'redux-saga'
+import { fork, put, take } from 'redux-saga/effects'
+
+import { fetchEventsActionMap, FETCH_EVENTS_ACTIONS } from 'ducks/events'
+import { fetchEvents } from 'api'
+
+function *fetchEventsFlow() {
+  while (true) {
+    try {
+      const { payload: {
+        currentDate,
+        by,
+      } } = yield take(FETCH_EVENTS_ACTIONS.REQUEST)
+      const payload = yield fetchEvents(currentDate, by)
+      yield put(fetchEventsActionMap.success(payload))
+    } catch (error) {
+      yield put(fetchEventsActionMap.failure())
+    }
+  }
+}
 
 export default function* rootSaga() {
-
+  yield fork(fetchEventsFlow)
 }
