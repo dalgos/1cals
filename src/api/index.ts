37d903e751fdf7dc0e587fea3c77c5cdf7ab2a4f
kwangs <kwangs@tmon.co.kr>
2019-10-31 15:33:51 +0900

    chore: api 업데이트
    * 월별 일정/주별 일정 조회하는 api 로 분할

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { startOfMonth, endOfMonth, getTime, startOfWeek, endOfWeek } from 'date-fns'
+import { compose } from 'ramda'
 
 interface EventsResponse {
   sid: number;
@@ -8,16 +9,39 @@ interface EventsResponse {
   title: string;
 }
 
+const getMinimumTimeByWeekly = compose(getTime, startOfWeek)
+const getMaximizeTimeByWeekly = compose(getTime, endOfWeek)
+/**
+ * 월간 일정 조회 기간 중 첫번째 시간
+ */
+const getMinimumTimeByMonthly = compose(getTime, startOfWeek, startOfMonth)
+/**
+ * 월간 일정 조회 기간 중 가장 마지막 시간
+ */
+const getMaximizeTimeByMonthly = compose(getTime, endOfWeek, endOfMonth)
+
 /**
  * 일정 목록을 조회한다.
  */
-export const fetchEvents = async (currentDate: Date, by: 'month' | 'week' = 'month'): Promise<EventsResponse> => {
-  const minimumTime = getTime(startOfWeek(startOfMonth(currentDate)))
-  const maximumTime = getTime(endOfWeek(endOfMonth(currentDate)))
-  console.log(startOfWeek(startOfMonth(currentDate)), endOfWeek(endOfMonth(currentDate)))
+export const fetchEvents = async (minTime: number, maxTime: number): Promise<EventsResponse> => {
   const { data } = await axios({
     method: 'GET',
-    url: `/api/events?startTime_gte=${minimumTime}&startTime_lte=${maximumTime}`
+    url: `/api/events?startTime_gte=${minTime}&startTime_lte=${maxTime}`
   })
+
   return data
 }
+
+export const fetchWeeklyEvents = async (currentDate: Date): Promise<EventsResponse> => {
+  const minimumTime = getMinimumTimeByWeekly(currentDate, undefined)
+  const maximumTime = getMaximizeTimeByWeekly(currentDate, undefined)
+  return await fetchEvents(minimumTime, maximumTime)
+}
+
+export const fetchMonthlyEvents = async (currentDate: Date): Promise<EventsResponse> => {
+  const minimumTime = getMinimumTimeByMonthly(currentDate)
+  const maximumTime = getMaximizeTimeByMonthly(currentDate)
+  return await fetchEvents(minimumTime, maximumTime)
+}
+
+
