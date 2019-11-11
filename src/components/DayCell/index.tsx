da177a0002426d34fb0ef9be6c50028b388b6f77
kwangs <kwangs@tmon.co.kr>
2019-11-11 17:34:18 +0900

    chore: 월간 뷰의 날짜 셀의 drop 이벤트 핸들러 추가

--- src/components/DayCell/index.tsx
+++ src/components/DayCell/index.tsx
@@ -8,19 +8,41 @@ interface Props {
   date: Date;
   day: string;
   onClick: (date: Date) => void;
+  onDrop: (eventData: string, targetDate: Date) => void;
 }
+
+type DragEventHandler = React.DragEventHandler<HTMLDivElement>
+type DropEventHandler = DragEventHandler
 
 export default function DayCell({
   children,
   day,
   date,
   onClick,
+  onDrop,
 }: Props): JSX.Element {
+  /**
+   * 날짜 셀 클릭 이벤트 핸들러
+   */
   const handleClick = () => onClick(date)
+  /**
+   * 이벤트 셀 드롭 이벤트 핸들러
+   * @param {React.DragEvent} evt
+   */
+  const handleDrop: DropEventHandler = (evt) => {
+    const eventData = evt.dataTransfer.getData('text')
+    onDrop(eventData, date)
+    evt.preventDefault()
+  }
+  const handleDragOver: DragEventHandler = (evt) => {
+    evt.preventDefault()
+  }
   return (
     <Cell
       activated={isToday(date)}
       onClick={handleClick}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
     >
       <h2>{day}</h2>
       <div>{children}</div>
